7234655d3b08912f2d1f861ebd30ebe94bbc35ef
Davi Lucciola <davilucciola14@gmail.com>
2025-05-17 20:42:56 -0300

    fix: removing api folder

--- src/app/api/posts/route.ts
+++
@@ -1,7 +0,0 @@
-// import { db } from "../../../../db";
-// import { NextRequest } from "next/server";
-
-// export function GET(req: NextRequest) {
-//   const { posts } = db;
-//   return Response.json(posts);
-// }
