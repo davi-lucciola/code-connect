0820cb89c750e483f61b09ca84fb7716d6c15ede
Davi Lucciola <davilucciola14@gmail.com>
2025-05-17 18:50:27 -0300

    [module2/class-3] - fixing pagination with prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,29 @@ type HomeProps = {
   searchParams: PaginationQuery;
 };
 
-async function getAllPosts(page: number): Promise<Pagination<Post[]>> {
+async function getAllPosts(
+  page: number,
+  perPage: number
+): Promise<Pagination<Post[]>> {
   try {
+    const totalItems = await db.post.count();
+    const totalPages = Math.ceil(totalItems / perPage);
+
+    const prev = page > 1 ? page - 1 : undefined;
+    const next = page < totalPages ? page + 1 : undefined;
+
     const posts = await db.post.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: "desc",
+      },
       include: {
         author: true,
       },
     });
-    return { data: posts };
+
+    return { data: posts, prev: prev, next };
   } catch (error) {
     logger.error("Falha ao obter posts", { error });
     return { data: [] };
@@ -25,8 +40,10 @@ async function getAllPosts(page: number): Promise<Pagination<Post[]>> {
 }
 
 export default async function HomePage({ searchParams }: HomeProps) {
-  const currentPage = searchParams.page || 1;
-  const { data: posts, prev, next } = await getAllPosts(currentPage);
+  const page = Number(searchParams.page) || 1;
+  const perPage = Number(searchParams.perPage) || 6;
+
+  const { data: posts, prev, next } = await getAllPosts(page, perPage);
 
   return (
     <main className={styles.main}>
@@ -34,8 +51,16 @@ export default async function HomePage({ searchParams }: HomeProps) {
         <CardPost key={post.id} post={post} />
       ))}
       <footer>
-        {prev && <Link href={`/?page=${prev}`}> Página Anterior </Link>}
-        {next && <Link href={`/?page=${next}`}> Proxima Página </Link>}
+        {prev && (
+          <Link href={`/?page=${prev}&perPage=${perPage}`}>
+            Página Anterior
+          </Link>
+        )}
+        {next && (
+          <Link href={`/?page=${next}&perPage=${perPage}`}>
+            Proxima Página
+          </Link>
+        )}
       </footer>
     </main>
   );
